ef89de2063e0f63856bef28f83ea8f083dcbef64
cosmin apreuetsei <cosmin.apreuetsei@gmail.com>
2013-02-04 21:15:30 +0200

    cairo + win32

---
+++ csrc/cairo/build-mingw32.sh
@@ -0,0 +1,39 @@
+
+
+./configure \
+	--enable-shared=no \
+	--enable-static=no \
+	--enable-xlib=no \
+	--enable-xcb=no \
+  --enable-qt=no \
+  --enable-quartz=no \
+  --enable-win32=yes \
+  --enable-win32-font=yes \
+  --enable-skia=no \
+  --enable-png=no \
+  --disable-png \
+  --disable-png=yes \
+  --enable-gl=no \
+  --enable-glesv2=no \
+  --enable-cogl=no \
+  --enable-vg=no \
+  --enable-egl=no \
+  --enable-glx=no \
+  --enable-wgl=no \
+  --enable-script=no \
+  --enable-ft=no \
+  --enable-fc=no \
+  --enable-ps=no \
+  --enable-pdf=yes \
+  --enable-svg=yes \
+  --enable-test-surfaces=no \
+  --enable-tee=no \
+  --enable-xml=no \
+  --enable-pthread=no \
+  --enable-gobject=no \
+  --enable-full-testing=no \
+  --enable-trace=no \
+  --enable-interpreter=no \
+  --enable-symbol-lookup=no \
+  --disable-some-floating-point=no
+
---
+++ csrc/cairo/cairo-features.h
@@ -0,0 +1,4 @@
+/* feature-enabling defines moved to build script */
+#ifndef CAIRO_FEATURES_H
+#define CAIRO_FEATURES_H 1
+#endif
