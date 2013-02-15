83b17304e6a25354c81e9773ecefb35cb038d309
cosmin apreuetsei <cosmin.apreuetsei@gmail.com>
2013-02-15 16:40:32 +0200

    path_shapes module refactoring

--- path_shapes.lua
+++ path_shapes.lua
@@ -1,10 +1,8 @@
 --conversion of 2d axes-aligned closed shapes to lines and curves
 
-local M = {}
-
 local kappa = 4 * ((math.sqrt(2) - 1) / 3) --http://www.whizkidtech.redprince.net/bezier/circle/
 
-function M.ellipse(write, cx, cy, rx, ry)
+local function ellipse(write, cx, cy, rx, ry)
 	local lx = rx * kappa
 	local ly = ry * kappa
 	write('move',  cx, cy-ry)
@@ -15,11 +13,11 @@ function M.ellipse(write, cx, cy, rx, ry)
 	write('close')
 end
 
-function M.circle(write, cx, cy, r)
-	M.ellipse(write, cx, cy, r, r)
+local function circle(write, cx, cy, r)
+	ellipse(write, cx, cy, r, r)
 end
 
-function M.rect(write, x1, y1, w, h)
+local function rectangle(write, x1, y1, w, h)
 	local x2, y2 = x1 + w, y1 + h
 	write('move', x1, y1)
 	write('line', x2, y1)
@@ -28,7 +26,7 @@ function M.rect(write, x1, y1, w, h)
 	write('close')
 end
 
-function M.round_rect(write, x1, y1, w, h, rx)
+local function round_rectangle(write, x1, y1, w, h, rx)
 	local ry = rx
 	local x2, y2 = x1 + w, y1 + h
 	local lx = rx * kappa
@@ -49,13 +47,20 @@ function M.round_rect(write, x1, y1, w, h, rx)
 end
 
 --a star has a center, two anchor points and a number of leafs
-function M.star(write, cx, cy, x1, y1, x2, y2, n)
+local function star(write, cx, cy, x1, y1, x2, y2, n)
 	error'NYI'
 end
 
 --a regular polygon has a center, a radius and a number of segments
-function M.regular_poly(write, cx, cy, n, r)
+local function regular_polygon(write, cx, cy, n, r)
 	error'NYI'
 end
 
-return M
+return {
+	ellipse = ellipse,
+	circle = circle,
+	rectangle = rectangle,
+	round_rectangle = round_rectangle,
+	star = star,
+	regular_polygon = regular_polygon,
+}
