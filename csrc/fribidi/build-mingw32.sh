ae548e3f7722bde552061610afaeb66e4e2acf2c
cosmin apreuetsei <cosmin.apreuetsei@gmail.com>
2013-06-09 19:01:59 +0300

    fribidi: added charset conversion functions and test

--- csrc/fribidi/build-mingw32.sh
+++ csrc/fribidi/build-mingw32.sh
@@ -1,3 +1,1 @@
-gcc -shared -O3 -s -o ../../bin/fribidi.dll -I. -Wall -ansi -DHAVE_CONFIG_H *.c
-
-#note: FRIBIDI_CHARSETS = 0, DEBUG = 0
+gcc -shared -O3 -s -o ../../bin/fribidi.dll -I. -Icharset -Wall -ansi -DHAVE_CONFIG_H *.c charset/*.c
