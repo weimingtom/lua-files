cfb4f4ed83d4cbf90e549902b0b19b24c738769a
cosmin apreuetsei <cosmin.apreuetsei@gmail.com>
2013-11-15 05:08:14 +0200

    dataset: sorting

--- dataset.lua
+++ dataset.lua
@@ -1,14 +1,15 @@
 --in-memory datasets by Cosmin Apreutesei (unlicensed)
 --features:
--- row-level change tracking with the ability to apply, merge or cancel changes
+-- row-level change tracking with the ability to apply, merge or cancel changes.
 -- apply changes with failure tracking and retry.
+-- sorting by multiple columns; row order is preserved with inserts and updates.
+--
 
 --[[
 TODO:
 - master-detail
 	- ?
 - grouping
-- sorting
 - undo/redo
 
 - memory model
@@ -27,7 +28,7 @@ local dataset = {}
 
 function dataset:new(fields)
 	self = glue.inherit(self)
-	self.fields = fields
+	self.fields = self:parse_fields(fields)
 	self:clear()
 	return self
 end
@@ -36,18 +37,27 @@ end
 function dataset:clear()
 	self.records = {}
 	self.deleted = {}
-	self.dirty = nil
+	self.row_order_lost = nil --flag indicating that original_row lost its meaning
 end
 
 function dataset:row_count()
 	return #self.records
 end
 
-function dataset:insert(row)
-	row = row or #self.records + 1
+function dataset:insert(row, record)
+	if self.sort_cmp then
+		row = self:sorted_row(record)
+	else
+		row = row or #self.records + 1
+	end
 	assert(row >= 1 and row <= #self.records + 1)
-	local record = {status = 'new'}
+	record = record or {}
+	record.status = 'new'
 	table.insert(self.records, row, record)
+end
+
+function dataset:append(record)
+	self:insert(#self.records + 1, record)
 end
 
 function dataset:delete(row)
@@ -70,6 +80,25 @@ function dataset:update(row)
 	end
 end
 
+function dataset:move(row1, row2)
+	assert(not self.sort_cmp)
+	assert(row1 >= 1 and row1 <= #self.records)
+	assert(row2 >= 1 and row2 <= #self.records + 1)
+	if row1 == row2 then return end
+	local record = table.remove(self.records, row1)
+	table.insert(self.records, row2, record)
+	self.row_order_lost = true
+end
+
+function dataset:updated(row) --TODO: change this API
+	assert(row >= 1 and row <= #self.records)
+	local record = self.records[row]
+	if not record.status then return end
+	if self.sort_cmp then
+		self:move(row, self:sorted_row(record))
+	end
+end
+
 --merge changes back into the datset: remove any traces of record change without applying the changes
 function dataset:merge()
 	for row, record in ipairs(self.records) do
@@ -77,16 +106,15 @@ function dataset:merge()
 		record.old = nil
 		record.original_row = row
 		record.fail = nil
+		record.fail_count = nil
 	end
 	self.deleted = {}
-	self.dirty = nil
+	self.row_order_lost = nil
 end
 
 --cancel changes: revert the dataset to the original state, before any changes were made
 function dataset:cancel()
 	assert(not self.dirty)
-	--TODO: unsort
-
 	--remove added records and revert updated ones
 	local i = 1
 	while self.records[i] do
@@ -100,24 +128,26 @@ function dataset:cancel()
 			i = i + 1
 		end
 	end
-	--add deleted records back to their original places
+	--add deleted records back to their original places, or at the end, if row order was lost
 	for i,record in ipairs(self.deleted) do
-		table.insert(self.records, record.original_row, record)
+		local row = self.row_order_lost and #self.records + 1 or record.original_row
+		table.insert(self.records, row, record)
 	end
 	--remove traces of record change
 	self:merge()
 end
 
---apply changes to the backend, and log any failures
-
 function dataset:exec(cmd, record) end --stub
 
+--apply changes to the backend, and log any failures
 function dataset:apply()
 	local fail_count = 0
+	local success_count = 0
 	for i = #self.deleted, 1, -1 do
 		local record = self.deleted[i]
 		if exec('delete', record) then
 			table.remove(self.deleted, i)
+			success_count = success_count + 1
 		else
 			record.fail = true
 			record.fail_count = (record.fail_count or 0) + 1
@@ -130,6 +160,7 @@ function dataset:apply()
 				record.status = nil
 				record.old = nil
 				record.original_row = row
+				success_count = success_count + 1
 			else
 				record.fail = true
 				record.fail_count = (record.fail_count or 0) + 1
@@ -137,43 +168,124 @@ function dataset:apply()
 			end
 		end
 	end
-	self.dirty = self.fail_count > 0
-	return not self.dirty, fail_count
+	self.row_order_lost = success_count > 0 and self.fail_count > 0
+	return self.fail_count == 0, fail_count, success_count
 end
 
---sort records
+--field-based access
 
-function dataset:sort(comp)
-	table.sort(self.records, comp)
-	--TODO: fix original_row
-end
-
---dataset with fields
-
-local function parse_fields(s)
-	local fields = {}
-	for s in glue.gsplit(s, ',') do
-		local field, dir = s:match'(.-)([%>%<]$'
-		table.insert(fields, field)
-	end
-	return fields
-end
-
-function dataset:sort_fields(fields)
+function dataset:parse_fields(fields)
 	if type(fields) == 'string' then
-		field = parse_fields(fields)
+		local t = {}
+		local i = 1
+		for name in glue.gsplit(fields, ',') do
+			name = glue.trim(name)
+			local field = {name = name, index = i}
+			t[name] = field
+			t[i] = field
+			i = i + 1
+		end
+		return t
+	else
+		return fields
 	end
-	self:sort(function(rec1, rec2)
-		--
-	end)
 end
 
---check unique keys
-function dataset:check_unique(row, values)
-	for i,s in ipairs(self.unique_key) do
+function dataset:col(field_name)
+	return self.fields[field_name].index
+end
 
+function dataset:get(row, field_name)
+	return self.records[row][self:col(field_name)]
+end
+
+function dataset:set(row, field_name, value)
+	assert(row >= 1 and row <= #self.records)
+	assert(self.records[row].status)
+	self.records[row][self:col(field_name)] = value
+end
+
+--given a list of fields and their direction, return a comparator function that compares two records.
+function dataset:comparator(arg)
+	local t = {} --{col1, 'asc' | 'desc', ...}
+	if type(arg) == 'string' then --'field1[:asc|:dsc],...'
+		for s in glue.gsplit(arg, ',') do
+			s = glue.trim(s)
+			local field, dir = s:match'^([^%:]+):?(.*)$'
+			assert(dir == '' or dir == 'asc' or dir == 'desc')
+			dir = dir == '' and 'asc' or dir
+			table.insert(t, self:col(field))
+			table.insert(t, dir)
+		end
+	else
+		for i = 1, #arg, 2 do
+			local field, dir = arg[i], arg[i+1]
+			assert(dir == 'asc' or dir == 'desc')
+			t[i], t[i+1] = self:col(field), dir
+		end
+	end
+	if #t == 2 then --simple case: sort by one column
+		local col, dir = t[1], t[2]
+		if dir == 'asc' then
+			return function(rec1, rec2)
+				return rec1[col] < rec2[col]
+			end
+		else
+			return function(rec1, rec2)
+				return rec2[col] < rec1[col]
+			end
+		end
+	end
+	local function cmp(rec1, rec2, i)
+		i = i or 1
+		if i > #t then
+			return false --all fields are equal
+		end
+		local col, dir = t[i], t[i+1]
+		if rec1[col] == rec2[col] then
+			return cmp(rec1, rec2, i + 2)
+		elseif dir == 'asc' then
+			return rec1[col] < rec2[col]
+		else
+			return rec2[col] < rec1[col]
+		end
+	end
+	return cmp
+end
+
+--sort by one or more fields (expressed as a string or table) or by a custom function
+function dataset:sort(arg)
+	local cmp
+	if type(arg) == 'function' then
+		cmp = arg
+	else
+		cmp = self:comparator(arg)
+	end
+	table.sort(self.records, cmp)
+	self.sort_cmp = cmp
+	self.row_order_lost = true
+end
+
+--binary search over sorted rows
+
+local function sorted_row_rec(rec, records, cmp, i, j)
+	local m = math.floor((i + j) / 2 + 0.5)
+	if cmp(rec, records[m]) then
+		return m == i and i or sorted_row_rec(rec, records, cmp, i, m - 1) --tail call
+	else
+		return m == j and j + 1 or sorted_row_rec(rec, records, cmp, m + 1, j) --tail call
 	end
 end
+
+local function sorted_row(rec, records, cmp)
+	if #records == 0 then return 1 end
+	return sorted_row_rec(rec, records, cmp, 1, #records)
+end
+
+function dataset:sorted_row(rec)
+	return sorted_row(rec, self.records, self.sort_cmp)
+end
+
 
 
 
@@ -211,6 +323,26 @@ ds:cancel()
 assert(not ds.records[1].old)
 assert(not ds.records[1].status)
 
+--sorting
+ds = dataset:new('id,name,descr')
+for i=40000,1,-4 do
+	ds:append{i-3,'foo','bla bla bla'}
+	ds:append{i-1,'bar','bla bla bla'}
+	ds:append{i-2,'bar','bla bla bla'}
+	ds:append{i-0,'zab','bla bla bla'}
+end
+assert(ds:row_count() == 40000)
+ds:merge()
+ds:sort('name:desc,id:desc')
+
+--row of sorted record
+local function cmp(a, b) return a < b end
+assert(sorted_row('a', {'b'}, cmp) == 1)
+assert(sorted_row('b', {'a'}, cmp) == 2)
+assert(sorted_row('a', {'a', 'b'}, cmp) == 2)
+assert(sorted_row('a', {'a', 'a', 'a', 'b'}, cmp) == 4)
+assert(sorted_row('b', {'a', 'c'}, cmp) == 2)
+assert(sorted_row('d', {'a', 'c'}, cmp) == 3)
 
 end
 
