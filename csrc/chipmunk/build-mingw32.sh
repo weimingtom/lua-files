56efc048fe8866ab2cb8f3256ea464d75f8c3340
cosmin apreuetsei <cosmin.apreuetsei@gmail.com>
2013-12-02 04:08:22 +0200

    chipmunk build script

--- csrc/chipmunk/build-mingw32.sh
+++ csrc/chipmunk/build-mingw32.sh
@@ -1,2 +1,3 @@
-# NOTE: pass -DNDEBUG to disable runtime assertions
-gcc -shared -O3 -s -o ../../bin/chipmunk.dll -Iinclude/chipmunk -std=gnu99 -Wall -ffast-math src/*.c src/constraints/*.c
+gcc -shared -O3 -s -o ../../bin/chipmunk.dll -Iinclude/chipmunk \
+	-std=gnu99 -Wall -ffast-math -DNDEBUG -DCHIPMUNK_FFI \
+	src/*.c src/constraints/*.c
